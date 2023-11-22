091ce17c357d57b6f9b730592c4d3523f7b12c9d
denis <deadkor26@gmail.com>
2023-11-22 19:05:04 +0200

    js -home

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,9 @@
 "use strict";
+function slugify(title) {
+    let slug = title.toLowerCase().replaceAll(" ", "-");
+    return slug;
+}
+console.log(slugify("Arrays for begginers"));
+console.log(slugify("English for developer"));
+console.log(slugify("Ten secrets of JavaScript"));
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
